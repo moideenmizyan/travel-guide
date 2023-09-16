82aedb284be945893b43723cca188324765351fb
moideenmizyan <36323788+moideenmizyan@users.noreply.github.com>
2023-09-17 03:58:38 +0530

    second commit

---
+++ src/components/PrivateRoute.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Route, Redirect, useLocation } from "react-router-dom";
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+    const user_data= JSON.parse(localStorage.getItem("user_data"));
+    const location=useLocation();
+
+  return (
+    <Route
+      {...rest}
+      render={(props) => {
+        if (user_data?.access) {
+          return <Component {...props} />;
+        } else {
+          return <Redirect to={{ pathname: "/auth/login", search: `?next=${location.pathname}` }} />;
+        }
+      }}
+    ></Route>
+  );
+};
+
+export default PrivateRoute;
--- src/components/screens/Places.js
+++ src/components/screens/Places.js
@@ -1,52 +1,126 @@
-import React from "react";
-import { Helmet } from "react-helmet";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useHistory } from "react-router-dom";
 import styled from "styled-components";
-import Header from "./includes/Header";
+import Header from "../screens/includes/Header";
+import axios from "axios";
+import { BASE_URL } from "../../axiosConfig";
+import Helmet from "react-helmet";
 
-const Places = () => {
-  return (
-    <>
-      <Helmet>
-        <title>Places | Travel Guide</title>
-      </Helmet>
-      <Header />
-      <TopContainer>
-        <Heading>Welcome John Doe</Heading>
-        <Paragraph>Explore the world around you</Paragraph>
-      </TopContainer>
-      <PlacesContainer>
-        <PlaceCard>
-          <PlaceCardLink>
-            <PLaceImage src="" alt="Image" />
-            <PlaceBottomContainer>
-              <PlaceTitle>Himalaya</PlaceTitle>
-              <Location>
-                <LocationIcon
-                  src={require("../../assets/images/place.svg").default}
-                  alt="Image"
-                ></LocationIcon>
-                <LocationName>Himalaya</LocationName>
-              </Location>
-            </PlaceBottomContainer>
-          </PlaceCardLink>
-        </PlaceCard>
-      </PlacesContainer>
-    </>
-  );
-};
+export default function Places() {
 
-const TopContainer = styled.div``;
-const Heading = styled.h1``;
-const Paragraph = styled.p``;
-const PlacesContainer = styled.ul``;
-const PlaceCard = styled.li``;
-const PlaceCardLink = styled(Link)``;
-const PLaceImage = styled.img``;
-const PlaceBottomContainer = styled.div``;
-const PlaceTitle = styled.h3``;
-const Location = styled.div``;
-const LocationIcon = styled.img``;
-const LocationName = styled.span``;
+    const history=useHistory();
+    const handleLogout = () => {
+        // updateUserData({ type: "LOGOUT" });
+        history.push("/auth/login/")
+      };
+    const [places, setPlaces] = useState([]);
 
-export default Places;
+    useEffect(() => {
+        axios
+            .get(`${BASE_URL}/places`)
+            .then((response) => {
+                setPlaces(response.data.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
+
+    const renderPlaces = () => {
+        return places.map((place) => (
+            <PlaceCard>
+                <PlaceCardLink to={`/place/${place.id}/`}>
+                    <PlaceImage src={place.image} alt="Image" />
+                    <PlaceBottomContainer>
+                        <PlaceTitle>{place.name}</PlaceTitle>
+                        <Location>
+                            <LocationIcon
+                                src={
+                                    require("../assets/images/place.svg")
+                                        .default
+                                }
+                                alt="Place"
+                            />
+                            <LocationName>{place.location}</LocationName>
+                        </Location>
+                    </PlaceBottomContainer>
+                </PlaceCardLink>
+            </PlaceCard>
+        ));
+    };
+
+    return (
+        <>
+            <Helmet>
+                <title>Places | Moke Travel</title>
+            </Helmet>
+            <Header />
+            <TopContainer>
+                <Heading>Welcome</Heading>
+                <Paragraph>Explore the world around you</Paragraph>
+            </TopContainer>
+
+            <PlacesContainer>{renderPlaces()}</PlacesContainer>
+            <PaginationContainer></PaginationContainer>
+        </>
+    );
+}
+
+const TopContainer = styled.div`
+    width: 90%;
+    margin: 100px auto 0;
+`;
+const Heading = styled.h1`
+    font-size: 36px;
+    margin-bottom: 20px;
+`;
+const Paragraph = styled.p`
+    font-size: 22px;
+    color: #dfdfe2;
+`;
+const PlacesContainer = styled.ul`
+    display: flex;
+    flex-wrap: wrap;
+    width: 90%;
+    margin: 50px auto 0;
+`;
+const PlaceCard = styled.li`
+    width: 23.5%;
+    margin-right: 2%;
+    margin-bottom: 25px;
+    &:nth-child(4n) {
+        margin-right: 0;
+    }
+`;
+
+const PlaceCardLink = styled(Link)`
+    display: block;
+    appearance: none;
+`;
+
+const PlaceImage = styled.img`
+    width: 100%;
+    display: block;
+    border-top-left-radius: 10px;
+    border-top-right-radius: 10px;
+`;
+const PlaceBottomContainer = styled.div`
+    padding: 10px 15px;
+`;
+const PlaceTitle = styled.h3`
+    margin-bottom: 10px;
+    font-size: 20px;
+`;
+const Location = styled.div`
+    display: flex;
+`;
+const LocationIcon = styled.img`
+    margin-right: 10px;
+`;
+const LocationName = styled.span`
+    font-size: 18px;
+`;
+const PaginationContainer = styled.div`
+    width: 90%;
+    margin: 50px auto 0;
+`;
